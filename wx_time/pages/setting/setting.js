6158fdea8822c18d295cf5ed46107b8b72fc05e4
19980801 <1252430435@qq.com>
2019-05-24 20:02:22 +0800

    log

--- wx_time/pages/setting/setting.js
+++ wx_time/pages/setting/setting.js
@@ -1,41 +1,80 @@
-var postsData = "../index/index.js"
+// pages/mine/mine.js
+var app = getApp()
 Page({
   data: {
-    switchopen: false
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    orderItems: [
+      {
+        typeId: 0,
+        name: '待付款',
+        url: 'bill',
+        imageurl: '../../images/person/personal_pay.png',
+      },
+      {
+        typeId: 1,
+        name: '待收货',
+        url: 'bill',
+        imageurl: '../../images/person/personal_receipt.png',
+      },
+      {
+        typeId: 2,
+        name: '待评价',
+        url: 'bill',
+        imageurl: '../../images/person/personal_comment.png'
+      },
+      {
+        typeId: 3,
+        name: '退换/售后',
+        url: 'bill',
+        imageurl: '../../images/person/personal_service.png'
+      }
+    ],
   },
-  onShow: function () {
-    this.setData({
-      workTime: wx.getStorageSync('workTime'),
-      restTime: wx.getStorageSync('restTime')
+  //事件处理函数
+  toOrder: function () {
+    wx.navigateTo({
+      url: '../order/order'
     })
   },
-  changeWorkTime: function (e) {
-    wx.setStorage({
-      key: 'workTime',
-      data: e.detail.value
-    })
-  },
-  changeRestTime: function (e) {
-    wx.setStorage({
-      key: 'restTime',
-      data: e.detail.value
-    })
-  },
-
-  onMusicTap: function (event) {
-    this.setData({
-      switchopen: !switchopen
-    })
-    var isRuning = postsData.isRuning;
-    if (isRuning && switchopen) {
-      wx.playBackgroundAudio({
-        dataUrl: 'http://zhangmenshiting.qianqian.com/data2/music/34f1a52820a66d12657f2827ee0fe78f/265723035/265723035.mp3?xcode=49043e2b2d4adc1bb123be2e4a685e68',
-        title: "大鱼",
+  onLoad: function () {
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
       })
-
+    } else if (this.data.canIUse) {
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
     }
-    else {
-      wx.pauseBackgroundAudio();
-    }
+  },
+  getUserInfo: function (e) {
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+  },
+  myAddress: function (e) {
+    wx.navigateTo({ url: '../addressList/addressList' });
   }
 })
